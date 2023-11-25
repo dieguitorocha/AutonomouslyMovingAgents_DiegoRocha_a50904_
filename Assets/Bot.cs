02a056053c6a8e2ae507ff8cefc82567936733cb
dieguitorocha <100223185+dieguitorocha@users.noreply.github.com>
2023-11-25 22:45:02 +0000

    Tutorial: 1.Complex Behaviours- Completo

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -133,7 +133,8 @@ public class Bot : MonoBehaviour
     {
         RaycastHit raycastInfo;
         Vector3 rayToTarget = target.transform.position - this.transform.position;
-        if(Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
+        float lookAngle = Vector3.Angle(transform.forward, rayToTarget);
+        if(lookAngle < 60 && Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
         {
             if (raycastInfo.transform.gameObject.tag == "cop")
                 return true;
@@ -141,11 +142,34 @@ public class Bot : MonoBehaviour
         return false;
 
     }
+
+    bool coolDown = false;
+    void BehaviourCooldawn()
+    {
+        coolDown = false;
+    }
+
+    bool CanSeeMe()
+    {
+        Vector3 rayToTarget = this.transform.position - target.transform.position;
+        float lookAngle = Vector3.Angle(target.transform.forward, rayToTarget);
+
+        if (lookAngle < 60)
+            return true;
+        return false;
+    }
     // Update is called once per frame
     void Update()
     {
-        if(CanSeeTarget())
-        
-        CleverHide();
+        if (!coolDown)
+        {
+            if (CanSeeTarget() && CanSeeMe())
+            {
+                CleverHide();
+                Invoke("BehaviourCoolDown", 5);
+            }
+            else
+                Pursue();
+        }
     }
 }
